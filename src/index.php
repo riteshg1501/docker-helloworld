0ace14bb6300f89f0f2a2c6ca97ca4821b857531
riteshg1501 <108603627+riteshg1501@users.noreply.github.com>
2022-09-26 23:19:20 +0530

    Update and rename index.html to index.php

---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Hello, World from Docker! <br>";
+echo "Hello ECS! <br>";
+echo '<img src="https://www.docker.com/wp-content/uploads/2022/03/horizontal-logo-monochromatic-white.png">';
+
+?>
